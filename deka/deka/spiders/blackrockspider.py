bbde26912e7dc6571a8397e68cf7544f57e7596b
Maxim Wagner <MaximWagnerWalldorf@live.de>
2024-09-26 21:25:53 +0200

    first draft

--- deka/deka/spiders/blackrockspider.py
+++ deka/deka/spiders/blackrockspider.py
@@ -5,4 +5,9 @@ class Blackrockspider(scrapy.Spider):
     start_urls = ['https://www.blackrock.com/de/privatanleger/markte/weekly-market-update']
 
     def parse(self, response):
-        pass
+        # blocks = response.css('div.para-content.col-xl-7.col-lg-9.col-12.ishares-remove-bootstrap-offset')
+
+        for x in response.css('div.para-content.col-xl-7.col-lg-9.col-12.ishares-remove-bootstrap-offset'):
+            yield{
+                'test' : x.css('p').get()
+            }
