289b2dc2603bc2c4f1a973e21ea3f30c5572efd0
Maxim Wagner <MaximWagnerWalldorf@live.de>
2024-10-17 12:29:42 +0200

    driver working fix

--- scraping_selenium/scraper.py
+++ scraping_selenium/scraper.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 import time
 
 
-service = Service(executable_path="geckodrover")
-driver = webdriver.Firefox(service=service)
+driver = webdriver.Firefox()
 
 
 driver.get("https://google.com")
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+driver.find_element(By.XPATH, "//div[text()='Accept all']").click()
+
+input_element.send_keys('moerfelden walldorf wochenmarkt' + Keys.ENTER)
 
 time.sleep(10)
 
